2aee861bd5c4735a21a2083fc73e13c309311853
Carlos Jimenez <74768457+Carlos-jim@users.noreply.github.com>
2025-03-29 00:33:50 -0400

    ...2

--- src/components/page/projectPage.tsx
+++ src/components/page/projectPage.tsx
@@ -3,7 +3,6 @@ import FluidSlider from "@/components/slider";
 export default function SliderProjects() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="text-4xl font-bold mb-8">Slider Fluido</h1>
       <FluidSlider />
     </main>
   )
